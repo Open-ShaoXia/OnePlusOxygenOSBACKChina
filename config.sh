e27061d4e778051e1eee560c1629b4fd7c397ea1
联盟少侠 <1121114685@qq.com>
2018-10-15 21:21:31 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -86,6 +86,8 @@ set_permissions() {
 
   # The following is default permissions, DO NOT remove
   set_perm_recursive  $MODPATH  0  0  0755  0644
+  set_perm_recursive  $MODPATH/system/app/baidushurufa  0  0  0755  0644
+  set_perm_recursive  $MODPATH/system/reserve/Weather  0  0  0755  0644
 }
 
 ##########################################################################################
